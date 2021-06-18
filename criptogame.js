6363b07954f4ddd666883dc6bcab4ef16cdc241c
alexacost <79877564+alexacost@users.noreply.github.com>
2021-06-18 20:04:53 -0300

    CRIPTOGAME

---
+++ criptogame.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+
+//     $("#section-one").click(function(){
+//         $("#section-one").hide();
+//         $("#section-two").css('display', 'flex').show();
+//       });
+    
+//       $("#button-addon2").click(function(){
+//         $("#section-two").hide();
+//         $("#section-three").css('display', 'flex').show();
+//       });
+    
+
+// });
